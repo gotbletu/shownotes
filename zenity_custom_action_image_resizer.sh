0aca597dc98af4f0741e1ca9b3df66ad7432913c
gotbletu <gotbletu@gmail.com>
2015-11-06 13:15:02 -0800

    resize rotate zenity v2

--- zenity_custom_action_image_resizer.sh
+++ zenity_custom_action_image_resizer.sh
@@ -21,7 +21,6 @@
 # note: %N is the selected filenames (without paths)
 # conditions: image files
 
-
 PICKSIZE=$(zenity --list --radiolist --height "255" --text "<b>Please</b> make a selection:" --hide-header --column "Pick" --column "Item" FALSE "96x96" FALSE "128x128" FALSE "640x480" FALSE "800x600" TRUE "1024x768" FALSE "1280x720" FALSE "1280x960" FALSE "1920x1080")
 
 myArray=( "$@" )
